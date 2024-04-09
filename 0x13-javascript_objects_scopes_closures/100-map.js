28d16e83135a331d7573361fc5712a3e509f2d6a
Teefah_Pearl <lateefatalao787@gmail.com>
2024-04-09 15:00:13 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-
-let list = require('./100-data').list;
-let newList = [];
-
-newList = list.map(function (n, i) {
-  return n * i;
-});
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
